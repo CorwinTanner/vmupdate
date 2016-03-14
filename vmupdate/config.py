dc2f8dd320999534fda9d2945249637c143d7c67
Corwin <CorwinTanner@gmail.com>
2016-03-14 09:28:37 -0500

    removed config extension

--- vmupdate/config.py
+++ vmupdate/config.py
@@ -2,11 +2,5 @@ import pkgutil
 import yaml
 
 
-def load_config(config_path=None):
-    config = yaml.load(pkgutil.get_data('vmupdate', 'config/vmupdate.yaml'))
-
-    if config_path:
-        with open(config_path, 'r') as config_file:
-            config.extend(yaml.load(config_file))
-
-    return config
+def load_config():
+    return yaml.load(pkgutil.get_data('vmupdate', 'config/vmupdate.yaml'))
