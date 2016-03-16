416d3c4543b6ef80e0e12ac9db6cbdb70af3e401
Corwin <CorwinTanner@gmail.com>
2016-03-16 15:28:48 -0500

    added connect and get_os methods and removed update method for vm

--- vmupdate/vm.py
+++ vmupdate/vm.py
@@ -1,3 +1,11 @@
+import platform
+
+from channel import Channel
+from config import config
+from credentials import get_credentials
+from shells import get_shell
+
+
 class VM:
     def __init__(self, virtualizer, uid):
         self.virtualizer = virtualizer
@@ -12,12 +20,28 @@ class VM:
     def get_status(self):
         return self.virtualizer.get_vm_status(self.uid)
 
+    def get_os(self):
+        return self.virtualizer.get_vm_os(self.uid)
+
     def get_ssh_info(self, ssh_port):
         return self.virtualizer.get_ssh_info(self.uid, ssh_port)
 
     def enable_ssh(self, host_port, guest_port):
         return self.virtualizer.enable_ssh(self.uid, host_port, guest_port)
 
-    def update(self, username, password):
-        # return self.pkgmgr.update(self.virtualizer, self.uid, username, password)
-        raise NotImplementedError()
+    def connect(self, ssh_port):
+        ip, port = self.get_ssh_info(ssh_port)
+
+        channel = Channel(ip, port)
+
+        username, password = get_credentials(self.uid)
+
+        channel.connect(username, password)
+
+        return get_shell(self.get_shell_name(), channel)
+
+    def get_shell_name(self):
+        if self.uid in config.machines and 'Shell' in config.machines[self.uid]:
+            return config.machines[self.uid]['Shell']
+
+        return config.shells.get(self.virtualizer.get_vm_os(self.uid))
