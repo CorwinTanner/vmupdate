c05a12f88402e3e21c2b3e8237755eefed3008af
Corwin <CorwinTanner@gmail.com>
2016-03-16 12:59:05 -0500

    updated command_exists from Posix to return bool

--- vmupdate/shells/posix.py
+++ vmupdate/shells/posix.py
@@ -8,4 +8,4 @@ class Posix(Shell):
     def command_exists(self, command):
         stdin, stdout, stderr = self.channel.run(['command', '-v', command])
 
-        return stdout.channel.recv_exit_status()
+        return stdout.channel.recv_exit_status() == 0
