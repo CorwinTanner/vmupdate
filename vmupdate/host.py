a5d8ed9b64da9d969808d9bdb92a5bce6294627b
Corwin <CorwinTanner@gmail.com>
2016-03-18 11:07:01 -0500

    updated get_all_vms to check and enable ssh

--- vmupdate/host.py
+++ vmupdate/host.py
@@ -2,20 +2,31 @@ import os
 import platform
 
 from config import config
-from virtualizers import get_virtualizer
+from virtualizers import get_virtualizer, VM_STOPPED
 from vm import VM
 
 
 def get_all_vms():
     vms = []
 
+    guest_ssh_port = config.network['SSH']['Guest']['Port']
+
     virtualizers = find_virtualizers()
+
+    available_ports = iter(get_available_ports(vms))
 
     for virt_name, virt_path in virtualizers.items():
         virtualizer = get_virtualizer(virt_name, virt_path)
 
         for vm_name, vm_uuid in virtualizer.list_vms():
-            vms.append(VM(virtualizer, vm_name))
+            vm = VM(virtualizer, vm_name)
+
+            ssh_info = vm.get_ssh_info(guest_ssh_port)
+
+            if not ssh_info and vm.get_status() == VM_STOPPED:
+                vm.enable_ssh(next(available_ports), guest_ssh_port)
+
+            vms.append(vm)
 
     return vms
 
