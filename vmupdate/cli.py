ec29c9e201afb45f741dd43c03ba610b1aa40d14
Corwin <CorwinTanner@gmail.com>
2016-03-19 10:03:26 -0500

    added main logic

--- vmupdate/cli.py
+++ vmupdate/cli.py
@@ -1,5 +1,30 @@
+import sys
+import time
+
+from config import config
+from host import get_all_vms
+from pkgmgr import get_pkgmgrs, run_pkgmgr
+from virtualizers import VM_STOPPED
+
+
 def main():
-    pass
+    config.load()
+
+    for vm in get_all_vms():
+        vm_orig_status = vm.get_status()
+
+        if vm_orig_status == VM_STOPPED:
+            vm.start()
+            time.sleep(config.general['Wait After Start'])
+
+        for pkgmgr, cmds in get_pkgmgrs(vm):
+            run_pkgmgr(vm, pkgmgr, cmds)
+
+        if vm_orig_status == VM_STOPPED:
+            vm.stop()
+
+    return 0
+
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
