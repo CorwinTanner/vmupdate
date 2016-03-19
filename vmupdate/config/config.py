99ce876231c39a08d1af76ee3cbfb9f8520f88a4
Corwin <CorwinTanner@gmail.com>
2016-03-19 15:14:36 -0500

    updated config to merge user file

--- vmupdate/config/config.py
+++ vmupdate/config/config.py
@@ -42,20 +42,44 @@ class Config(ConfigSection):
     def machines(self):
         return self._machines
 
-    def load(self):
-        self._config = yaml.load(pkgutil.get_data('vmupdate', 'data/vmupdate.yaml'))
+    def load(self, config_path=None):
+        default_config = yaml.load(pkgutil.get_data('vmupdate', 'data/vmupdate.yaml'))
 
-        self._general = General(self._config['General'])
-        self._credentials = Credentials(self._config['Credentials'])
-        self._network = Network(self._config['Network'])
-        self._virtualizers = Virtualizers(self._config['Virtualizers'])
-        self._pkgmgrs = PackageManagers(self._config['Package Managers'])
-        self._shells = Shells(self._config['Shells'])
-        self._machines = Machines(self._config['Machines'])
+        if config_path:
+            with open(config_path, 'r') as config_file:
+                user_config = yaml.load(config_file)
+
+            self._data = merge(default_config, user_config)
+        else:
+            self._data = default_config
+
+        self._general = General(self._data['General'])
+        self._credentials = Credentials(self._data['Credentials'])
+        self._network = Network(self._data['Network'])
+        self._virtualizers = Virtualizers(self._data['Virtualizers'])
+        self._pkgmgrs = PackageManagers(self._data['Package Managers'])
+        self._shells = Shells(self._data['Shells'])
+        self._machines = Machines(self._data['Machines'])
 
         self._logging = yaml.load(pkgutil.get_data('vmupdate', 'data/logging.yaml'))
 
         logging.config.dictConfig(self._logging)
+
+
+def merge(a, b):
+    if not b:
+        return a
+
+    if isinstance(a, dict):
+        for key in b:
+            if key in a:
+                a[key] = merge(a[key], b[key])
+            else:
+                a[key] = b[key]
+    else:
+        a = b
+
+    return a
 
 
 config = Config()
